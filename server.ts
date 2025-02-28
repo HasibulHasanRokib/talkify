6b30f358d9b7d25b25c8c3b869cea6dc01c82460
hasibulhasanrokib <rokib4000@gmail.com>
2025-02-28 10:14:09 +0600

    feat: implement online user management with Socket.io; add user connection and disconnection handling

--- server.ts
+++ server.ts
@@ -2,7 +2,8 @@ import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 import { Server } from "socket.io";
-
+import { Profile } from "@prisma/client";
+import { db } from "./lib/prisma";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -13,6 +14,35 @@ app.prepare().then(() => {
   const httpServer = createServer((req, res) => {
     const parsedUrl = parse(req.url!, true);
     handle(req, res, parsedUrl);
+  });
+  const io = new Server(httpServer);
+
+  interface IOnlineUser {
+    socketId: string;
+    profile: Profile;
+  }
+
+  let onlineUsers: IOnlineUser[] = [];
+
+  io.on("connection", (socket) => {
+    //add user
+    socket.on("add-new-user", async (userId: string) => {
+      if (!userId) return;
+
+      const profile = await db.profile.findFirst({ where: { userId } });
+      if (!profile) return;
+
+      if (!onlineUsers.some((user) => user.profile.id === profile.id)) {
+        onlineUsers.push({ socketId: socket.id, profile });
+      }
+      io.emit("get-users", onlineUsers);
+    });
+
+    //filter active user
+    socket.on("disconnect", () => {
+      onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+      io.emit("get-users", onlineUsers);
+    });
   });
 
   httpServer
